be7162afc2ae72892f8fe7d615abbfbbbe293791
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-10-28 12:15:56 +0100

    add seeder,controlle,model,migration passenger

--- app/Models/Trains.php
+++ app/Models/Trains.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Trains extends Model
 {
     use HasFactory;
+
+    public function getDate(){
+        $date = Carbon::create($this->date, "Europe/Rome");
+        return $date->toDateString();
+    }
 }
--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -3,7 +3,29 @@
 @section("head-title", "Trains")
 
 @section("main-content")
-<h1>
-    Trains
-</h1>
+<div class="container text-center">
+<h2>Monitor</h2>
+<div class="row g-3">
+@forelse ($trains as $train )
+<div class="col-4">
+    <div class="card" >
+    <h4>{{$train->train_cd}}</h4>
+    <div class="card-body">
+        <h5 class="card-title">Data</h5>
+        <p class="card-text">{{$train->date}}</p>
+      <h5 class="card-title">Stazioni</h5>
+      <div class="stations d-flex justify-content-center"><p class="card-text">Partenza: {{$train->station_departure}}</p>---<p class="card-text">Arrivo: {{$train->station_arrive}}</p></div>
+      <h5 class="card-title">orari</h5>
+      <div class="stations d-flex justify-content-center"><p class="card-text">Partenza: {{$train->hour_departure}}</p>---<p class="card-text">Arrivo: {{$train->hour_arrive}}</p></div>
+
+    </div>
+  </div>
+</div>
+@empty
+    <div class="col-12">
+        <h2>Non ci sono treni in partenza....</h2>
+    </div>
+@endforelse
+</div>
+</div>
 @endsection

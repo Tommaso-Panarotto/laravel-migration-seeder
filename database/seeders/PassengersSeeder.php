89bee9125604598f197aeda7f4312f6682e96c57
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-10-28 15:51:29 +0100

    seeder passengers

--- database/seeders/PassengersSeeder.php
+++ database/seeders/PassengersSeeder.php
@@ -2,16 +2,23 @@
 
 namespace Database\Seeders;
 
+use App\Models\Passenger;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class PassengersSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i=0; $i<50;$i++){
+        $passengers = new Passenger();
+        $passengers->name = $faker->firstName();
+        $passengers->surname = $faker->lastName();
+        $passengers->save();
+        }
     }
 }

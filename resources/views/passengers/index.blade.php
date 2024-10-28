56cc7744e90544b342ffd35c9e32489e4588ce8d
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-10-28 15:56:22 +0100

    show in page all passengers

--- resources/views/passengers/index.blade.php
+++ resources/views/passengers/index.blade.php
@@ -5,5 +5,19 @@
 @section("main-content")
 <div class="container text-center">
 <h2>Passengers</h2>
+<div class="row g-3 mt-5">
+@forelse ($passengers as $passenger )
+<div class="col-4">
+    <div class="card" >
+      <h4>{{$passenger->name}} {{$passenger->surname}}</h4>
+    </div>
+  </div>
+
+@empty
+    <div class="col-12">
+        <h2>Non ci sono passegeri in partenza....</h2>
+    </div>
+@endforelse
+</div>
 </div>
 @endsection
